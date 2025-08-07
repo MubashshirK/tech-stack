d2b6d56ad5240043c7595308846d6185461e174b
Mubashshir Khan <mubashshirk786@gmail.com>
2025-08-07 22:25:48 +0530

    login fixes

--- components/HomeBanner.tsx
+++ components/HomeBanner.tsx
@@ -7,7 +7,7 @@ import { banner_1 } from '@/images'
 const HomeBanner = () => {
   return (
     <div className='py-16 md:py-0 bg-gray-200 rounded-lg pl-10 lg:pl-24 flex items-center justify-between'>
-      <div className='space-y-5'>
+      <div className='space-y-5 sm:pr-16'>
         <Title>Get Upto 50% Off on Selected Tech Products</Title>
         <Link href={"/shop"} className='bg-shop_dark_green/90 text-white/90 px-5 py-2 rounded-md text-sm font-semibold hover:text-white hover:bg-shop_dark_green hoverEffect'>Buy Now</Link>
       </div>
