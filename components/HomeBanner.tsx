5194f35deaab2e83fcf3edf212f49c848b82374f
Mubashshir Khan <mubashshirk786@gmail.com>
2025-08-10 14:44:57 +0530

    style changes

--- components/HomeBanner.tsx
+++ components/HomeBanner.tsx
@@ -8,8 +8,8 @@ const HomeBanner = () => {
   return (
     <div className='py-16 md:py-0 bg-gray-200 rounded-lg pl-10 lg:pl-24 flex items-center justify-between'>
       <div className='space-y-5 sm:pr-16'>
-        <Title>Get Upto 50% Off on Selected Tech Products</Title>
-        <Link href={"/shop"} className='bg-shop_dark_green/90 text-white/90 px-5 py-2 rounded-md text-sm font-semibold hover:text-white hover:bg-shop_dark_green hoverEffect'>Buy Now</Link>
+        <Title className='pr-10'>Get Upto 50% Off on Selected Tech Products</Title>
+        <Link href={"/shop"} className='bg-shop_light_green/90 text-white/90 px-5 py-2 rounded-md text-sm font-semibold hover:text-white hover:bg-shop_dark_green hoverEffect'>Buy Now</Link>
       </div>
       <div>
         <Image src={banner_1} alt='banner_1' className='hidden md:inline-flex w-80' />
